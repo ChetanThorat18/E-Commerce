2d1d3c453f431c9211863c28889f9e61ed84bca7
ChetanThorat18 <chetanthorat146@gmail.com>
2024-06-07 19:44:07 +0530

    Handled Filtering

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -6,21 +6,21 @@ import { Suspense } from "react";
 
 const ListPage = async ({ searchParams }: { searchParams: any }) => {
   const wixClient = await wixClientServer();
-  const categoryResponse = await wixClient.collections.getCollectionBySlug(
+
+  const cat = await wixClient.collections.getCollectionBySlug(
     searchParams.cat || "all-products"
   );
 
   return (
-    <div className="px-4 md:px-8 lg:px:16 xl:px:32 2xl:px-64 relative">
+    <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
       {/* CAMPAIGN */}
       <div className="hidden bg-pink-50 px-4 sm:flex justify-between h-64">
         <div className="w-2/3 flex flex-col items-center justify-center gap-8">
           <h1 className="text-4xl font-semibold leading-[48px] text-gray-700">
             Grab up to 50% off on
-            <br />
-            Selected Products
+            <br /> Selected Products
           </h1>
-          <button className="rounded-3xl bg-lama text-white w-max py-3 px-5 text-sm ">
+          <button className="rounded-3xl bg-lama text-white w-max py-3 px-5 text-sm">
             Buy Now
           </button>
         </div>
@@ -28,16 +28,14 @@ const ListPage = async ({ searchParams }: { searchParams: any }) => {
           <Image src="/woman.png" alt="" fill className="object-contain" />
         </div>
       </div>
-
       {/* FILTER */}
       <Filter />
       {/* PRODUCTS */}
-      <h1 className="mt-12 text-xl font-semibold">Shoes for you</h1>
+      <h1 className="mt-12 text-xl font-semibold">{cat?.collection?.name} For You!</h1>
       <Suspense fallback={"loading..."}>
         <ProductList
           categoryId={
-            categoryResponse.collection?._id ||
-            "00000000-000000-000000-000000000001"
+            cat.collection?._id || "00000000-000000-000000-000000000001"
           }
           searchParams={searchParams}
         />
