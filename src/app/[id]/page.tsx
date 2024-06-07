0cca7409b2f496804746b42b1f01063ecfb8f8f6
ChetanThorat18 <chetanthorat146@gmail.com>
2024-06-07 12:34:32 +0530

    Product Quantity and Add to cart backend

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,47 +1,76 @@
 import Add from "@/components/Add";
 import CustomizeProducts from "@/components/CustomizeProducts";
 import ProductImages from "@/components/ProductImages";
+import { wixClientServer } from "@/lib/wixClientServer";
+import { notFound } from "next/navigation";
 
-const SinglePage = () => {
+type Params = {
+  id: string;
+};
+
+const SinglePage = async ({ params }: { params: Params }) => {
+  // Fetching the Wix client
+  const wixClient = await wixClientServer();
+
+  console.log(params);
+
+  // Querying the product using the slug from the params
+  const products = await wixClient.products
+    .queryProducts()
+    .eq("slug", params.id) // Using the dynamic id from the URL
+    .find();
+
+  if (!products.items[0]) {
+    return notFound();
+  }
+
+  const product = products.items[0];
+
   return (
     <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative flex flex-col lg:flex-row gap-16">
       {/* IMG */}
       <div className="w-full lg:w-1/2 lg:sticky top-20 h-max">
-        <ProductImages />
+        <ProductImages items={product.media?.items} />
       </div>
       {/* TEXT */}
       <div className="w-full lg:w-1/2 flex flex-col gap-6">
-        <h1 className="text-4xl font-medium">Product Name</h1>
-        <p className="text-gray-500">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet dicta
-          quaerat voluptatem beatae necessitatibus ullam cumque soluta suscipit,
-          dolore ex odio veritatis.
-        </p>
-        <div className="h-[2px] bg-gray-100"/>
-        <div className="flex items-center gap-4">
-            <h3 className="text-xl text-gray-500 line-through">$49</h3>
-            <h2 className="font-md text-2xl">$44</h2>
-        </div>
-        <div className="h-[2px] bg-gray-100"/>
-        <CustomizeProducts/>
-        <Add/>
+        <h1 className="text-4xl font-medium">{product.name}</h1>
+        <p className="text-gray-500">{product.description}</p>
+        <div className="h-[2px] bg-gray-100" />
+        {product.price?.price === product.price?.discountedPrice ? (
+          <h2 className="font-medium text-2xl">₹{product.price?.price}</h2>
+        ) : (
+          <div className="flex items-center gap-4">
+            <h3 className="text-xl text-gray-500 line-through">
+              ₹{product.price?.price}
+            </h3>
+            <h2 className="font-medium text-2xl">
+              ₹{product.price?.discountedPrice}
+            </h2>
+          </div>
+        )}
+        <div className="h-[2px] bg-gray-100" />
+        {product.variants && product.productOptions ? (
+          <CustomizeProducts
+            productId={product._id!}
+            variants={product.variants}
+            productOptions={product.productOptions}
+          />
+        ) :(
+          <Add
+          productId={product._id!}
+          variantId="00000000-0000-0000-0000-000000000000"
+          stockNumber={product.stock?.quantity || 0}
+        />
+      )}
+        <div className="h-[2px] bg-gray-100" />
 
-        <div className="h-[2px] bg-gray-100"/>
-
-        <div className="text-sm">
-            <h4 className="font-medium mb-4">Title</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, impedit labore! Molestiae natus provident esse a, soluta dolore voluptates fugiat. Magni sed, vel voluptas itaque vitae alias repudiandae, id exercitationem voluptates quos consequuntur.</p>
-        </div>
-        <div className="text-sm">
-            <h4 className="font-medium mb-4">Title</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, impedit labore! Molestiae natus provident esse a, soluta dolore voluptates fugiat. Magni sed, vel voluptas itaque vitae alias repudiandae, id exercitationem voluptates quos consequuntur.</p>
-        </div>
-        <div className="text-sm">
-            <h4 className="font-medium mb-4">Title</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, impedit labore! Molestiae natus provident esse a, soluta dolore voluptates fugiat. Magni sed, vel voluptas itaque vitae alias repudiandae, id exercitationem voluptates quos consequuntur.</p>
-        </div>
-
-
+        {product.additionalInfoSections?.map((section: any) => (
+          <div className="text-sm" key={section.title}>
+            <h4 className="font-medium mb-4">{section.title}</h4>
+            <p>{section.description}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
